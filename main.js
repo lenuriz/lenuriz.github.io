d513389f60af019401046b7f063a2fc69d26b7ce
batya <lenur.izetov@gmail.com>
2020-05-30 03:16:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+Vue.component('task',{
+  props: ['data'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    task_done(){
+      this.$emit('task_done')
+    }
+  },
+  template: `
+  <div class="task">
+    <div>
+      <h3 class="task__title">{{data.title}}</h3>
+    </div>
+    <button class="task__done" @click="task_done()">✔️</button>
+  </div>`
+})
+
+var vue = new Vue({
+  el: '#app',
+  data: {
+    string: 'asdasda',
+    new_task: {
+      title: ''
+    },
+    tasks : [
+      
+    ]
+  },
+  methods: {
+    add_task(){
+      if(this.new_task.title != ''){
+        this.tasks.push({
+          title: this.new_task.title
+        });
+        this.new_task.title='';
+      }
+    },
+    delete_task(id){
+      this.tasks.splice(id,1);
+    }
+  }
+})
